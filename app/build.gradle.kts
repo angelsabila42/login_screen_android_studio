e5f411a40e4104bed35df8987fd06d1b5be3d8a4
Tabitha Chebet <tabithasabila48@gmail.com>
2026-03-02 11:05:47 +0300

    Edited the setContent() and preview functions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,12 @@ plugins {
 
 android {
     namespace = "com.github.tabithachebet.loginscreen"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.github.tabithachebet.loginscreen"
         minSdk = 28
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/github/tabithachebet/loginscreen/MainActivity.kt
+++ app/src/main/java/com/github/tabithachebet/loginscreen/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -20,9 +21,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             LoginScreenTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    LogInScreen(
+                        paddingValues = innerPadding
                     )
                 }
             }
@@ -31,17 +31,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LogInScreen(paddingValues: PaddingValues) {
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LogInScreenPreview() {
     LoginScreenTheme {
-        Greeting("Android")
+        LogInScreen(paddingValues = PaddingValues())
     }
 }
